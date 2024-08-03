462c59f25454105ddb613b98d1b019781a991f6a
sardorazimov <sardorazimov>
2024-08-04 01:14:55 +0300

    a

--- components/example/ExampleHero.tsx
+++ components/example/ExampleHero.tsx
@@ -1,15 +1,12 @@
-// components/Hero.js
+'use clinet'
 import { GridBeam } from './GridBeam';
-import SvgGrid from './Svg';
 
 
 const Hero = () => (
   <div className="relative w-full h-screen px-12 ">
-    <SvgGrid>
-      <div>
-        helo
-      </div>
-    </SvgGrid>
+
+     
+  
   </div>
 );
 
